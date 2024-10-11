e0d000c6470a3795359b8b994814ac49961b2f41
kaushikbuddy28 <161143506+kaushikbuddy28@users.noreply.github.com>
2024-10-11 15:39:39 +0530

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -31,7 +31,7 @@ export default function Intro() {
             }}
           >
             <Image
-              src="https://www.canva.com/design/DAGSO_1btj8/Sw1hr_StZLpV-WN2i3PDfw/view?utm_content=DAGSO_1btj8&utm_campaign=designshare&utm_medium=link&utm_source=editor"
+              src="https://photos.google.com/photo/AF1QipMXWrZAawYCMJayE2LdXh4h84qPBURgoCx6wxp5"
               alt="KAUSHIK CHATURVEDI"
               width="192"
               height="192"
